342b24fb848bea2e1dc203c49d56c1aa8238e4fb
wbdana <william.b.dana@gmail.com>
2017-08-21 14:09:05 -0400

    Initial setup; not working

--- src/components/LineShowPage.js
+++ src/components/LineShowPage.js
@@ -3,6 +3,8 @@ import Line from "./Line"
 
 import { Container } from 'semantic-ui-react'
 
+import ActionCable from '../../node_modules/actioncable/lib/assets/compiled/action_cable.js'
+
 class LineShowPage extends React.Component {
 
   componentWillMount(){
