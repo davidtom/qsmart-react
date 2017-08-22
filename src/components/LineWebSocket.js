5ed93da7dad4fc0712190bf976a04d49496c6863
wbdana <william.b.dana@gmail.com>
2017-08-22 01:19:47 -0400

    Quick fix

--- src/components/LineWebSocket.js
+++ src/components/LineWebSocket.js
@@ -6,20 +6,18 @@ import App from '../App'
 class LineWebSocket extends React.Component {
   constructor(props) {
     super(props)
-
-    this.state = {
-      lineId: this.props['data-lineData']
-    }
   }
 
   // I think the problem might be in the ...{room: ____} line -- repeatedly shows up as undefined. Need to pass in the room ID here. Action Cable is broadcasting the line object to "line_channel_2", e.g., if lineId === 2, which corresponds to {channel: "LineChannel", room: 2}
+
+
   componentDidMount() {
     console.log(this.props['data-getLineData'])
     this.props['data-getLineData'](window.location.href.match(/\d$/)[0])
     this.setState({
       lineId: (window.location.href.match(/\d$/)[0])
     })
-    this.props['data-cableApp'].line = this.props['data-cableApp'].cable.subscriptions.create({channel: "LineChannel", room: "2"}, {
+    this.props['data-cableApp'].line = this.props['data-cableApp'].cable.subscriptions.create({channel: "LineChannel", room: window.location.href.match(/\d$/)[0]}, {
       received: (line) => {
         this.props['data-updateApp'](line)
       }
