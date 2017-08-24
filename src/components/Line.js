739dd15809e95fe82d8d23619437e07c96a76ec2
davidtom <davidjtomczyk@gmail.com>
2017-08-23 20:52:39 -0400

    logout redirects to main page

--- src/components/Line.js
+++ src/components/Line.js
@@ -34,7 +34,7 @@ class Line extends React.Component{
       this.setState({
         isChecked: !this.state.isChecked
       })
-    }    
+    }
     })
   }
 
@@ -48,12 +48,7 @@ class Line extends React.Component{
         <Radio toggle checked={this.state.isChecked === '' ? this.props.lineData.line.active :this.state.isChecked } label='Active' onClick={()=>this.onClick(this.props.lineData.line.id)} />
         <Divider section hidden clearing={true}/>
         <SectionHeader title={`Code: ${this.props.lineData.line.code}`}/>
-          <Transition.Group
-              animation="fly left"
-              duration={1000}
-          >
           {this.displayLineMembers()}
-        </Transition.Group>
       </Segment>
     )
 
